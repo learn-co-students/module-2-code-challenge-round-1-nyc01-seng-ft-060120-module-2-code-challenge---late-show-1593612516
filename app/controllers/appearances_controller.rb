eb5361e00bffe15a589f731b3566c59da75b7786
maxcroy1 <max.croy1@gmail.com>
2020-07-01 11:28:11 -0400

    I forgot to commit as I went... Final Solution

---
+++ app/controllers/appearances_controller.rb
@@ -0,0 +1,27 @@
+class AppearancesController < ApplicationController
+
+    def new
+        @appearance = Appearance.new
+    end
+
+    def create
+        @appearance = Appearance.create(appearance_params)
+        if @appearance.valid?
+            redirect_to episode_path(@appearance.episode)
+        else
+            flash[:errors] = @appearance.errors.full_messages
+            redirect_to new_appearance_path
+        end
+    end
+
+    # Possibly not needed
+    def show
+        @appearance = Appearance.find(params[:id])
+    end
+
+    private
+    def appearance_params
+        params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    end
+
+end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,8 @@
 class Guest < ApplicationRecord
+    has_many :appearances
+    has_many :episodes, through: :appearances
 
+    def sorted_appearances
+        self.appearances.sort_by{|a| a.rating}.reverse
+    end
 end
