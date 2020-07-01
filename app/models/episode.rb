891972cee2d7fa67f7f1bf9431958c51678f4112
shiro-han <shan99business@gmail.com>
2020-07-01 12:03:01 -0400

    deliverables

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,18 @@
 class Episode < ApplicationRecord
+    has_many :appearances
+    has_many :guests, through: :appearances
+
+    def find_rating(guest)
+        self.appearances.find_by(guest: guest).rating
+    end
+
+    def avg_rating
+        if self.appearances.count == 0
+            nil
+        else
+            self.appearances.map do |appear|
+                appear.rating
+            end.sum/self.appearances.count
+        end
+    end
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,8 @@
 class Guest < ApplicationRecord
+    has_many :appearances
+    has_many :episodes, through: :appearances
 
+    def sorted_episodes
+        self.episodes.sort_by{|e| e.find_rating(self)}.reverse!
+    end
 end
