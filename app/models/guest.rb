a44e644ed2db490085099d4700ac0c9798dec860
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-07-01 11:58:28 -0400

    i think i did all of it question mark

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,21 +2,15 @@ class Guest < ApplicationRecord
     has_many :appearances
     has_many :episodes, through: :appearances
 
-    def appearance_rating
-        self.appearance.rating
-    end
 
-    # def episode_date_and_rating
-    #     byebug
-    #     episode = self.episodes.each do |episode|
-    #         episode
-    #     end
-    #     appearance = self.appearances.each do |appearance|
-    #         appearance
-    #     end
-    #     if episode.id = appearance.episode.id
-    #         puts "#{episode.date}, #{appearance.rating}"
-    #     end
-    # end
+    def episode_date_and_rating
+        self.appearances.each do |appearance|
+            self.episodes.each do |episode|
+                if appearance.episode_id == episode.id 
+                    return "#{episode.date}, #{appearance.rating}"
+                end
+            end
+        end
+    end
 
 end
