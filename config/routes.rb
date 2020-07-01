b9f8a2a50ce0a81d888005c12bfcbdac9862e743
Leamsi Escribano <leamsi.esc@gmail.com>
2020-07-01 11:52:44 -0400

    cleaning up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :guests, only: [:index, :show]
   resources :episodes, only: [:index, :show]
-
-  # TODO: Limit these paths
   resources :appearances, only: [:new, :create]
 end
