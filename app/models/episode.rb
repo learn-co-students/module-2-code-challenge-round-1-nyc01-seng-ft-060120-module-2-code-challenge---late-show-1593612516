dda939e8ba8f6ed4b99ebab869c82b18c2dfcd89
dr-dolce14 <dolcemascop1@gmail.com>
2020-07-01 11:35:19 -0400

    number 7 done

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,5 +1,14 @@
 class Episode < ApplicationRecord
     has_many :appearances
     has_many :guests, through: :appearances
-    
+
+    def avg_rating
+    a = self.appearances.map do |appearance|
+        appearance.rating
+    end
+    if a.sum(0.0) > 0
+        a.sum(0.0) / a.size.to_f
+    end
+    end
+
 end
