252c7c77a8dc0c509f3f3e92c95b3bad71217672
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-07-01 10:36:07 -0400

    new form for appearances

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,3 +1,16 @@
 class AppearancesController < ApplicationController
-    
+    def new
+        @appearance = Appearance.new 
+    end
+
+    def create
+        @appearance = Appearance.create(appearance_params)
+        redirect_to episode_path(@appearance.episode_id)
+    end
+
+    private
+
+    def appearance_params
+        params.require(:appearance).permit(:episode_id, :guest_id, :rating)
+    end
 end
