23221f1466605ab315acdfe3992219224eb6d13f
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-07-01 11:30:05 -0400

    cant remember if i committed with the aditional appearance validation so im doing this. also have the episode average rating

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,18 @@
 class Guest < ApplicationRecord
     has_many :appearances
     has_many :episodes, through: :appearances
+
+    # def episode_date_and_rating
+    #     byebug
+    #     episode = self.episodes.each do |episode|
+    #         episode
+    #     end
+    #     appearance = self.appearances.each do |appearance|
+    #         appearance
+    #     end
+    #     if episode.id = appearance.episode.id
+    #         puts "#{episode.date}, #{appearance.rating}"
+    #     end
+    # end
+
 end
