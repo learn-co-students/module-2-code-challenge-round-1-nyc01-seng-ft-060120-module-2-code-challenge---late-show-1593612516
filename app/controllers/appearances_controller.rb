21dfb0847294bd5dd0cfdcbc615bdd69345e5549
dr-dolce14 <dolcemascop1@gmail.com>
2020-07-01 11:17:57 -0400

    4 and 5 done

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -8,7 +8,12 @@ class AppearancesController < ApplicationController
 
     def create
     @appearance = Appearance.create(appearance_params)
-    redirect_to episode_path(@appearance.episode.id)
+    if @appearance.valid?
+     redirect_to episode_path(@appearance.episode.id)
+    else   
+        flash[:my_errors] = @appearance.errors.full_messages 
+        redirect_to new_appearance_path 
+    end
     end
 
     private
--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,4 +1,8 @@
 class Appearance < ApplicationRecord
   belongs_to :guest
   belongs_to :episode
+
+  validates :rating, presence: true
+  validates :rating, numericality: true
+  validates :rating, inclusion: {in: (1..5) }
 end
