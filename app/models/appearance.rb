038c48f3e0aecda302290c4d74f037a06aed75dd
RitaHartnell <mel.soriano6123@gmail.com>
2020-07-01 11:43:41 -0400

    a couple of extra deliverables working

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -2,5 +2,10 @@ class Appearance < ApplicationRecord
     belongs_to :guest
     belongs_to :episode
     validates :rating, :inclusion => 1..5
+    validate :guest_cannot_appear_more_than_once
 
+
+    def guest_cannot_appear_more_than_once
+        errors.add(:guest, "cannot appear more than once") if Appearance.where({guest: guest, episode: episode}).exists?
+    end
 end
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,16 @@
 class Episode < ApplicationRecord
     has_many :appearances
     has_many :guests, through: :appearances
+
+    def average_rating
+        average = 0
+        if self.appearances != []
+            self.appearances.each do |a|
+                average += a.rating
+            end
+            average = average/self.appearances.count
+        else
+            return nil
+        end
+    end
 end
