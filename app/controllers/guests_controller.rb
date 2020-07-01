df6ed48e327e715b88b6c72c792d5813fc72c370
RitaHartnell <mel.soriano6123@gmail.com>
2020-07-01 11:49:39 -0400

    last deliverable

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -6,5 +6,6 @@ class GuestsController < ApplicationController
 
   def show
     @guest = Guest.find(params[:id])
+    @sorted = @guest.appearance_sort
   end
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,8 @@
 class Guest < ApplicationRecord
     has_many :appearances
     has_many :episodes, through: :appearances
+
+    def appearance_sort
+        self.appearances.sort_by{|appearance| appearance.rating}
+    end
 end
